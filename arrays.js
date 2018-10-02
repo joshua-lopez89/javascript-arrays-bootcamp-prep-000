5ecd6b7b92650e59c7c0b4ddbef1131c17bab028
Joshua S Lopez <joshua.lopez.89@gmail.com>
2018-10-02 21:51:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,8 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
-function addElementToBeginningOfArray = chocolateBars.unshift("foo")
+function addElementToBeginningOfArray = chocolateBars.unshift('foo')
+
+
 
 
 
