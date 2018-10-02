62a5bd86fe7ad716709f0f1e66bf8782ea8a110b
Joshua S Lopez <joshua.lopez.89@gmail.com>
2018-10-02 21:18:17 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+
+var addElementToBeginningOfArray = ["butterfinger", ...chocolateBars];
+
+var destructivelyAddElementsToBeginningOfArray = 
+chocolateBars.upshift("hershey");
+
+
+
+
