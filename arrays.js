47f2833357dded89316d8b9c273dce13143163cb
Joshua S Lopez <joshua.lopez.89@gmail.com>
2018-10-02 22:13:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,10 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {array.unshift(element)
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+
 
 
 
